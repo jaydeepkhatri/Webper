2eef5d987cfa886127c4a32bdc5d61e74175a027
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2022-08-14 20:30:11 +0530

    ✏ Style updates

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,12 +1,13 @@
 import { FiSearch } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import { Status, ResponseHeaders, Config, Loading } from "../index";
+import backgroundImg from "../../assets/img/background.jpg";
 import axios from "axios";
 
 const Search = () => {
     //testing API without CORS
-    const [search, setSearch] = useState('https://animechan.vercel.app/api/randdom');
-    // const [search, setSearch] = useState('http://localhost:3000/');
+    const [search, setSearch] = useState('https://jsonplaceholder.typicode.com/todos');
+    //const [search, setSearch] = useState('http://localhost:3000/');
     //const [search, setSearch] = useState('');
     const [webdata, setWebData] = useState({});
     const [error, setError] = useState(false);
@@ -33,7 +34,7 @@ const Search = () => {
 
     return (
             <div className="section">
-                <div className="inputcontainer">
+                <div className="inputcontainer" style={{backgroundImage: `url(${backgroundImg})`}}>
                     <div className="form">
                         <input type="text" className="input" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="https://facebook.com" />
                         <button type="submit"><FiSearch /></button>
@@ -44,7 +45,20 @@ const Search = () => {
                 {
                     isLoading ? <Loading /> :
                     error ? 
-                        <p>Oppsie, We have a error</p>
+                        <>
+                            <div className="content">
+                                <div className="box">
+                                    <div className="title">Oppsie, We have a error..</div>
+                                    <pre>{JSON.stringify(webdata.response.data, null, 2)}</pre>
+                                </div>
+                                <div className="info">
+                                    <Status status={webdata.response.status} />
+                                    <ResponseHeaders responseheader={webdata.response.headers} />
+                                    <Config config={webdata.response.config} />
+
+                                </div>
+                            </div>
+                        </>
                     : <div className="content">
                         <div className="box">
                             <div className="title">Data Response</div>
