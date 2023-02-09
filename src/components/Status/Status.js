4b81add660da885119716d4d193e17598271854b
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2023-02-09 16:07:37 +0530

    ✏ Fix header col

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -18,7 +18,7 @@ const Status = () => {
 				<div className="title__wrapper">
 					<p className="title">Info</p>
 				</div>
-				<p><span>Status:</span>:<span className={statusColor}>{status}</span></p>
+				<p><span>Status:</span>:<span className={statusColor}>{webdata.status}</span></p>
 				<p><span>Time:</span>:<span>{timeToLoad}ms</span></p>
 				<p><span>URL:</span>:<span>{search}</span></p>
 				<p><span>Data Length:</span>:<span>{dataSize}</span></p>
