c448ab622c2fa51e888c1bbd801eb8d2e51a9210
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2023-02-25 12:09:11 +0530

    ✏ update default search url

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { useState, createContext } from 'react';
 export const AppContext = createContext(null);
 
 function App() {
-	//const [search, setSearch] = useState('https://jethalal-quotes.vercel.app/v1');
+	const [search, setSearch] = useState('https://jethalal-quotes.vercel.app/v1');
 	//const [search, setSearch] = useState('https://jsonplaceholder.typicode.com/todos?_limit=4');
 	//const [search, setSearch] = useState('http://localhost:3000/');
-	const [search, setSearch] = useState('https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80');
+	//const [search, setSearch] = useState('https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80');
 
 	const [webdata, setWebData] = useState({});
 	const [error, setError] = useState(false);
